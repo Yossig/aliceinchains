9cc7353f3ca23cbaa102f88a1ba4d1ef96470e9f
Yossi Gartner <yosigartner@gmail.com>
2018-08-22 23:44:52 +0300

    add fonts, index.js add css features for digital number

---
+++ index.js
@@ -0,0 +1,45 @@
+var shockLevel = 1;
+
+function changeLevel(direction) {
+    var validate = direction + shockLevel;
+    if(!(validate > 14 || validate < 1)) {
+        shockLevel += direction;
+    }
+
+    updateView();
+}
+
+function updateView() {
+    $(".shock-level").text(shockLevel);
+}
+
+function shockAction(){
+    postAction('{actiontype:"shock", level:'+shockLevel+'}');
+}
+
+function vibrateAction(){
+    postAction('{action:"vibrate"}')
+}
+
+function postAction(action) {
+    /*$.post("http://192.168.0.14:1234/postAction", 
+    {
+        action:action
+    })
+    .fail(function() {
+        alert("Could'nt connect to server");
+    })*/
+    $.ajax({
+        type:"POST",
+        url: "http://192.168.0.14:1234/postAction",
+        data: action,
+        error: function(){
+            alert("Could'nt connect to server")
+        },
+        timeout: 3000
+    })
+}
+
+$(document).ready(function() {
+    updateView();
+});
